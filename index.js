0009f384522062db502af67cd7280b32d4ee9cd5
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-05-04 12:55:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,96 @@
 // code your solution here
+/*Write a function called superbowlWin() in index.js:
+
+The function should receive 1 argument, an Array of JavaScript Objects
+Each object has two properties: year and result
+It should use find() to test each Object to see if the result is "W" — a win!
+It should return the year when the win occurred (if it occurred at all!)
+If no win is found, it should return, sadly, undefined */
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+    { year: "2006", result: "N/A"},
+    { year: "2005", result: "N/A"},
+    { year: "2004", result: "N/A"},
+    { year: "2003", result: "N/A"},
+    { year: "2002", result: "N/A"},
+    { year: "2001", result: "N/A"},
+    { year: "2000", result: "N/A"},
+    { year: "1999", result: "N/A"},
+    { year: "1998", result: "W"},
+    { year: "1997", result: "W"},
+    { year: "1996", result: "N/A"},
+    { year: "1995", result: "N/A"},
+    { year: "1994", result: "N/A"},
+    { year: "1993", result: "N/A"},
+    { year: "1992", result: "N/A"},
+    { year: "1991", result: "N/A"},
+    { year: "1990", result: "N/A"},
+    { year: "1989", result: "L"},
+    { year: "1988", result: "N/A"},
+    { year: "1987", result: "L"},
+    { year: "1986", result: "L"},
+    { year: "1985", result: "N/A"},
+    { year: "1984", result: "N/A"},
+    { year: "1983", result: "N/A"},
+    { year: "1982", result: "N/A"},
+    { year: "1981", result: "N/A"},
+    { year: "1980", result: "N/A"},
+    { year: "1979", result: "N/A"},
+    { year: "1978", result: "N/A"},
+    { year: "1977", result: "N/A"},
+    { year: "1976", result: "L"},
+    { year: "1975", result: "N/A"},
+    { year: "1974", result: "N/A"},
+    { year: "1973", result: "N/A"},
+    { year: "1972", result: "N/A"},
+    { year: "1971", result: "N/A"},
+    { year: "1970", result: "N/A"},
+    { year: "1969", result: "N/A"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"},
+    { year: "1966", result: "N/A"},
+    { year: "1965", result: "N/A"},
+    { year: "1964", result: "N/A"},
+    { year: "1963", result: "N/A"},
+    { year: "1962", result: "N/A"},
+    { year: "1961", result: "N/A"},
+    { year: "1960", result: "N/A"}
+  ]
+
+  //using find()
+      
+  function superbowlWin(array){
+    const printYear = array.find(element => element.result === "W");
+    return printYear.year;
+  } 
+  console.log(superbowlWin(record));//2015
+
+//working but dont give me undifiend
+/*const printYear = record.find(superbowlWin).year;
+function superbowlWin(array){
+    return array.result  === 'W';
+} 
+console.log(printYear);*/
+
+
+
+    //using for loop 
+    /*function superbowlWin1(array){
+        for(let item of array){
+            if(item.result === "W"){
+                return item.year;
+            }
+        }
+    }
+    console.log(superbowlWin1(record))*/ //2015
